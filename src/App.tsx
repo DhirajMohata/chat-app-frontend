93dfbb906a73073f938edd66846e47f3d52ac0f3
DhirajMohata <dhirajmohata86@gmail.com>
2024-08-18 22:21:59 +0530

    i

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard  from './components/Dashboard';
-import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
