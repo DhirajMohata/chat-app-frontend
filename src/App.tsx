c0dbd37297d19ad5c2614c3b4204c88117dfb8ca
DhirajMohata <dhirajmohata86@gmail.com>
2024-08-18 22:28:29 +0530

    i

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -8,17 +8,15 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <BrowserRouter>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/chat" element={<Dashboard />} />
-        {/* <Route path="*" element={<div>Not Found</div>} /> */}
+        <Route path="*" element={<div>Not Found</div>} />
       </Routes>
     </Router>
-    </BrowserRouter>
   );
 }
 
