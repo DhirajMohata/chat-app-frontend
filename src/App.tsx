f1f474e2b81d275fb91a84808ec1e515358d1edb
DhirajMohata <dhirajmohata86@gmail.com>
2024-08-19 10:32:59 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard  from './components/Dashboard';
@@ -10,7 +9,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/chat" element={<Dashboard />} />
